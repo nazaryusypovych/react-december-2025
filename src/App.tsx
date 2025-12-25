635dfe86313abd98b26b092890ffc5b52ebdbdff
nazaryusypovych <mega.bismen@gmail.com>
2025-12-25 14:01:35 +0200

    Add useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
 import './App.css'
+import {useState} from "react";
 
 function App() {
 
+    let [counter, setCounter] = useState(0);
+
+    console.log(`mount`)
+
   return (
     <>
+        <h2>{counter}</h2>
+
+      <button onClick={()=>{
+          setCounter(++counter);
+      }}>increment</button>
+
+
+      <button onClick={()=>{
+          setCounter(prevState => {
+              return --prevState
+          });
+      }}>decrement</button>
 
     </>
   )
