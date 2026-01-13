06d237b2ab3716921e3e2db5d60af912fb98e1ba
nazaryusypovych <mega.bismen@gmail.com>
2026-01-13 22:38:51 +0200

    Homework 3 - task 1

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,21 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import {createBrowserRouter, RouterProvider} from "react-router";
+import HomePage from "./page/HomePage.tsx";
+
+
+const router = createBrowserRouter([
+    {path: `/`, element: <HomePage/>,
+        children:[
+            {path: `users`, element: <div>Users page</div>},
+            {path: `posts`, element: <div>Posts page</div>},
+            {path: `comments`, element: <div>Comments page</div>},
+            {path: `products`, element: <div>Products page</div>}
+        ]
+    }
+])
+
 
 createRoot(document.getElementById('root')!).render(
-    <App />
+    <RouterProvider router={router}/>
 )
